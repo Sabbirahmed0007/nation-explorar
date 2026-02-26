d208457bd78f07dbc4ee91ad71123bcfc01442fb
Sabbirahmed0007 <www.sabbirahmed3071@gmail.com>
2026-02-26 17:29:18 +0600

    show the data in the ui

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -12,7 +12,7 @@ const Countries = ({ countriesPromise }) => {
   return (
     <div>
       <h1>Countries: {countries.length}</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-5'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-5 my-10'>
         {
           countries.map(country=><Country country={country} key={country.id}></Country>)
         }
--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 
 const Country = ({ country }) => {
-    
+
 
     console.log(country.capital.capital[0]);
     console.log(country);
-    const{capital, flags, name}=country
-    
-    
-    
-    
+    const { capital, flags, name ,region ,population} = country
+
+
+
+
 
 
     return (
-        <div>
-            <div>
-                <img src={flags.flags.png} alt={flags.flags.alt} />
+        <div className='flex items-center justify-between   shadow-2xl border-2 p-2 bg-amber-200 rounded-2xl mx-5'>
+            <div className='h-16 w-24 '>
+                <img className='h-full w-full rounded-2xl' src={flags.flags.png} alt={flags.flags.alt} />
+
+            </div>
+            <div className='relative'>
+                <select className='absolute right-0 w-36 overflow-auto'>
+                    <option value="name">{ name.common}</option>
+                    <option value="bangladesh" className='overflow-auto'>{ name.official}</option>
+                    <option value="capital">Capital: {capital.capital[0]}</option>
+                    <option value="region">Region: { region.region}</option>
+                    <option value="region">Populaton: { population.population}</option>
+                </select>
+
             </div>
         </div>
     );
