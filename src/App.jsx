5bbced68efcbd3871eff0702459fb27ec2c9512b
Sabbirahmed0007 <www.sabbirahmed3071@gmail.com>
2026-02-26 16:06:26 +0600

    on the way to explore the world with react . set up the app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+
+  
+
+  return (
+    <>
+      
+      <Countries></Countries>
+      
+        
+      
+     
+    </>
+  )
+}
+
+export default App
